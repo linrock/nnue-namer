a9fa8c5dae09b0a4efd8791471690471e1cdf707
Linmiao Xu <linmiao.xu@gmail.com>
2023-03-28 23:09:11 -0400

    Rename and default concurrency to cores minus 1

--- cpu_nnue_namer.py
+++ cpu_nnue_namer.py
@@ -1,12 +1,10 @@
 import hashlib
-from multiprocessing import Process, Value
+from multiprocessing import cpu_count, Process, Value
 import secrets
 import sys
 
-NUM_PROCESSES = 4
-
 if len(sys.argv) != 3:
-    print('Usage: ./multi_cpu_nnue_namer.py <nnue_filename> <hex_word_list>')
+    print('Usage: ./cpu_nnue_namer.py <nnue_filename> <hex_word_list>')
     sys.exit(0)
 
 def get_nnue_data(nnue_filename):
@@ -33,13 +31,13 @@ def find_variants(nnue_filename, hex_word_list, counter):
         sha256_prefix = sha256[:12]
         with counter.get_lock():
             counter.value += 1
-        if any(word in sha256_prefix for word in hex_word_list):
+        if any(sha256_prefix.startswith(word) for word in hex_word_list):
             print(f'Found {sha256_prefix} after {counter.value} tries')
             new_nnue_filename = f'nn-{sha256_prefix}.nnue'
             print(f'Writing nnue data to {new_nnue_filename}')
             with open(new_nnue_filename, 'wb') as f:
                 f.write(nnue_data)
-        elif counter.value % 100 == 0:
+        elif counter.value % 10000 == 0:
             print(f'Tried {counter.value} times')
 
 nnue_filename = sys.argv[1]
@@ -47,7 +45,7 @@ hex_word_list = open(sys.argv[2], 'r').read().strip().split('\n')
 counter = Value('i', 0)
 processes = [
     Process(target=find_variants, args=(nnue_filename, hex_word_list, counter))
-    for i in range(NUM_PROCESSES)
+    for i in range(cpu_count() - 1)
 ]
 for p in processes: p.start()
 for p in processes: p.join()
