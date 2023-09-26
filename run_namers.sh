96845fb8fc29b741091874c4f3adb22f0ae14295
Linmiao Xu <linmiao.xu@gmail.com>
2023-09-26 11:16:47 -0400

    Script for many different single-core namer instances

---
+++ run_namers.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "Usage: ./run_namers.sh <nnue_filename>"
+  exit 0
+fi
+
+nnue=$1
+num_workers=8
+
+for core in $(seq 0 $(( $num_workers - 1 ))); do
+  taskset -c $core python3 fast_cpu_nnue_namer.py $nnue hexword-list.txt 1 &
+done
+
+wait
