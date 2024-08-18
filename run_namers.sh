01673b11a16521f713bac3535dedbd83ed692ada
Linmiao Xu <linmiao.xu@gmail.com>
2024-08-18 00:40:16 -0400

    Lower cpu priority

--- run_namers.sh
+++ run_namers.sh
@@ -9,7 +9,7 @@ nnue=$1
 num_workers=$(( $(nproc) - 1 ))
 
 for core in $(seq 0 $(( $num_workers - 1 ))); do
-  taskset -c $core python3 fast_cpu_nnue_namer.py $nnue hexword-list.txt 1 &
+  nice -n 15 taskset -c $core python3 fast_cpu_nnue_namer.py $nnue hexword-list.txt 1 &
 done
 
 wait
